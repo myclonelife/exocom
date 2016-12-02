85507a196528dd9fcd20392281bc024bdb6376c9
Martin Jaime <martin.jaime-viveros@originate.com>
2016-12-02 09:27:28 -0800

    Updates exorelay-go to pass tests

--- exorelay-go/exorelay/exorelay.go
+++ exorelay-go/exorelay/exorelay.go
@@ -3,24 +3,62 @@ package exorelay
 import (
 	"errors"
 	"fmt"
+
+	uuid "github.com/satori/go.uuid"
+
+	"golang.org/x/net/websocket"
 	// HandlerManager
 	// MessageSender
-	// ZmqListener
 )
+
+type Message struct {
+	Name         string      `json:"name,omitempty"`
+	Sender       string      `json:"sender,omitempty"`
+	Payload      interface{} `json:"payload,omitempty"`
+	ResponseTo   string      `json:"responseTo,omitempty"`
+	ID           string      `json:"id,omitempty"`
+	Timestamp    int         `json:"timestamp,omitempty"`
+	ResponseTime int         `json:"timestamp,omitempty"`
+}
 
 type Relay struct {
 	ServiceName string
 	ExocomHost  string
 	ExocomPort  int
+	websocket   *websocket.Conn
 }
 
-func NewRelay(ServiceName, ExocomHost string, ExocomPort int) *Relay {
-	return &Relay{ServiceName, ExocomHost, ExocomPort}
+func New(ServiceName, ExocomHost string, ExocomPort int) (*Relay, error) {
+	ws, err := websocket.Dial(url(ExocomHost, ExocomPort), "", origin(ExocomHost))
+	if err != nil {
+		return nil, err
+	}
+	message := Message{
+		Name:    "exocom.register-service",
+		Sender:  ServiceName,
+		Payload: fmt.Sprintf("name: \"%s\"", ServiceName),
+		ID:      uuid.NewV4().String(),
+	}
+	websocket.JSON.Send(ws, message)
+	return &Relay{ServiceName, ExocomHost, ExocomPort, ws}, err
 }
 
-func (relay *Relay) Send(message string) error {
-	fmt.Println("Message: " + message)
-	return errors.New("Send is unimplemented\n")
+func (relay *Relay) Close() error {
+	return relay.websocket.Close()
+}
+
+func (relay *Relay) Send(name, payload string) (string, error) {
+	if payload == "" && name == "" {
+		return "", errors.New("ExoRelay.Send cannot send empty messages")
+	}
+	message := Message{
+		Name:    name,
+		Sender:  relay.ServiceName,
+		Payload: payload,
+		ID:      uuid.NewV4().String(),
+	}
+	err := websocket.JSON.Send(relay.websocket, message)
+	return message.ID, err
 }
 
 func (relay *Relay) OnIncomingMessage(request string) (string, error) {
@@ -31,4 +69,21 @@ func (relay *Relay) OnIncomingMessage(request string) (string, error) {
 func (relay *Relay) SendRoutingConfig() error {
 	fmt.Println("SendRoutingConfig is unimplemented")
 	return errors.New("SendRoutingConfig is unimplemented\n")
+}
+
+func origin(host string) string {
+	return fmt.Sprintf("http://%s/", host)
+}
+
+func url(host string, port int) string {
+	return fmt.Sprintf("ws://%s:%d/services", host, port)
+}
+
+func NewMessage(name, sender, payload string) Message {
+	return Message{
+		Name:    name,
+		Sender:  sender,
+		Payload: payload,
+		ID:      uuid.NewV4().String(),
+	}
 }
